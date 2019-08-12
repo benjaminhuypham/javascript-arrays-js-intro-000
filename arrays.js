c50a5cd85dc37cbefce0a5920906a6f74d952fd7
Huy Pham <benjaminhuypham@gmail.com>
2019-08-12 19:40:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 var addElementToBeginningOfArray= function(array, element){
-  return array.unshift(element); 
+  return array.unshift(element);
 }
